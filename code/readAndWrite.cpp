af81aa1439ffdee8afcde499261dd15169583df9
gabialverga <gabrielaalverga@gmail.com>
2018-10-01 20:21:09 -0300

    Read and Write in C++ :)

---
+++ code/readAndWrite.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int number;
+    float anotherNumber;
+    string name;
+    cout<<"Number: "<<endl;
+    cin>>number;
+    cout<<"Another number: "<<endl;
+    cin>>anotherNumber;
+    cout<<"Name: "<<endl;
+    cin>>name;
+    return 0;
+}

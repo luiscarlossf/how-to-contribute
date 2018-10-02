8d992b0ad48457843f7ea9bf4f06fe4971b49e69
Achiv Chauhan <achivchauhan@gmail.com>
2018-10-02 18:14:07 +0530

    Added js switch example

---
+++ code/getDay.js
@@ -0,0 +1,10 @@
+switch (new Date().getDay()) {
+    case 6:
+        text = "Today is Saturday";
+        break; 
+    case 0:
+        text = "Today is Sunday";
+        break; 
+    default: 
+        text = "Looking forward to the Weekend";
+}

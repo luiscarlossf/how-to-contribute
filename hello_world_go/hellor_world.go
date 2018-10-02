4f192ae49dbebd3c926b6a07ed3ec5fac3f5da3d
Luis Carlos <luiscarlos.sf@outlook.com>
2018-10-02 00:21:42 -0300

    Delete hellor_world.go

--- hello_world_go/hellor_world.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-import "gopkg.in/kyokomi/emoji.v1"
-
-func main() {
-	pizzaMessage := emoji.Sprint("Hello World, Hacktoberfest :beer: Beer!!!")
-	fmt.Println(pizzaMessage)
-}
